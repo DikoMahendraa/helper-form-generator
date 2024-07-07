5eb42962ea8a710f547fe63922d0cd837ecf974d
DikoMahendraa <diko.dev99@gmail.com>
2024-07-07 08:52:59 +0700

    fix: responsive header

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,7 +8,7 @@ const LandingPage = () => {
       <header className="py-4">
         <div className="container mx-auto flex flex-wrap justify-between items-center px-4">
           <div className="text-3xl font-bold">Form Builder</div>
-          <nav className="mt-4 sm:mt-0">
+          <nav className="lg:mt-4 mt-0 hidden lg:inline">
             <Link to="#" className="block sm:inline-block text-white mr-4">
               Home
             </Link>
